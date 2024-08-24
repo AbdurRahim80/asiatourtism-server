a1879253bb1a10e060e4693f476990e6424835cd
AbdurRahim80 <mdrahim.nil2005@gmail.com>
2024-08-24 12:46:02 -0700

    add delete of crud and data delete in mongoDB

--- index.js
+++ index.js
@@ -83,7 +83,12 @@ async function run() {
       res.send(result)
     })
 
-    
+    app.delete('/spot/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await userCollection.deleteOne(query)
+      res.send(result);
+    })
 
     
     
