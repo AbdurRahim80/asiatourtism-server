2f1c0d4fc33f8fbaf27ce14da9ff9498c60df098
AbdurRahim80 <mdrahim.nil2005@gmail.com>
2024-08-25 04:05:07 -0700

    add vercel configuration

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -12,8 +13,8 @@ app.use(express.json());
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = "mongodb+srv://asiatourtism:wuxPevXUU2ucyl6a@cluster0.qgxn6jx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-// const uri = "mongodb+srv://<username>:<password>@cluster0.qgxn6jx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+// const uri = "mongodb+srv://asiatourtism:wuxPevXUU2ucyl6a@cluster0.qgxn6jx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qgxn6jx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -27,7 +28,6 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
 
     const userCollection = client.db("touristSpotDB").collection("touristSpot");
 
